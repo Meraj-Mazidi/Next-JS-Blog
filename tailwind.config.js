2eee0c5d26072b08e5b13a7640fbeb9b507da276
Meraj-Mazidi <merajmazidi@gmail.com>
2022-11-05 19:42:52 +0330

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -7,9 +12,6 @@ module.exports = {
         "2/3": "66.666667%",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
